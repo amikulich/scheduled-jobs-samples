c652469beba63c6430783e59f848f3f465f818be
Artem Mikulich <artem.mikulich@amobee.com>
2020-05-30 21:44:38 +0300

    01 Initial State

---
+++ src/01/Reports/Api/Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportDataSource _reportDataSource;
+
+        public ReportsController(IReportDataSource reportDataSource)
+        {
+            _reportDataSource = reportDataSource;
+        }
+
+        [HttpGet]
+        [Route("clients/{clientId}/daily-report")]
+        public IActionResult Get(int clientId)
+        {
+            var result = _reportDataSource.GetByClient(clientId);
+
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ src/01/Reports/Api/ReportDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Api
+{
+    public class ReportDataSource : IReportDataSource
+    {
+        public ReportData GetByClient(int clientId)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            return new ReportData()
+            {
+                ClientId = clientId,
+                Info = $"report for {clientId}"
+            };
+        }
+    }
+
+    public interface IReportDataSource
+    {
+        ReportData GetByClient(int clientId);
+    }
+
+    public class ReportData
+    {
+        public int ClientId { get; set; }
+
+        public string Info { get; set; }
+    }
+}
